a7993e00dce264e69d20b0472f3da27a6b2f05ff
Georgina5-2 <122113060+Georgina5-2@users.noreply.github.com>
2023-06-28 15:42:34 -0400

    link to resume changed

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -20,7 +20,7 @@ const backEndSkills = [
 const Resume = () => {
     return (
         <div className="post-body d-flex-col">
-            <h4 className="d-flex justify-content-center p-3 font-os font-pink font-large">View my resume <a href="https://drive.google.com/drive/u/0/folders/1a_YlAqlE_8XlpPjd8JFIjhvmg1uEczBo" target="_blank" rel="noreferrer" className="text-decoration-none" >&nbsp;here</a></h4>
+            <h4 className="d-flex justify-content-center p-3 font-os font-pink font-large">View my resume <a href="./components/assets/MERN-Resume.pdf" target="_blank" rel="noreferrer" className="text-decoration-none" >&nbsp;here</a></h4>
             <div className="d-flex justify-content-evenly">
                 <div className="pt-3">
                     <h3 className="font-os font-pink">Front-end Skills</h3>

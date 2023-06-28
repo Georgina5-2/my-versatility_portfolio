a51ee864bca98a54f74c78efef9b7880e735a2b9
Georgina5-2 <122113060+Georgina5-2@users.noreply.github.com>
2023-06-28 15:54:05 -0400

    file path for resume changed

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -20,7 +20,7 @@ const backEndSkills = [
 const Resume = () => {
     return (
         <div className="post-body d-flex-col">
-            <h4 className="d-flex justify-content-center p-3 font-os font-pink font-large">View my resume <a href="./components/assets/MERN-Resume.pdf" target="_blank" rel="noreferrer" className="text-decoration-none" >&nbsp;here</a></h4>
+            <h4 className="d-flex justify-content-center p-3 font-os font-pink font-large">View my resume <a href="https://drive.google.com/file/d/1lKmxGoMiRD7vE_yjxfFgIEJdI98k5anB/view?usp=drive_link" target="_blank" rel="noreferrer" className="text-decoration-none" >&nbsp;here</a></h4>
             <div className="d-flex justify-content-evenly">
                 <div className="pt-3">
                     <h3 className="font-os font-pink">Front-end Skills</h3>
